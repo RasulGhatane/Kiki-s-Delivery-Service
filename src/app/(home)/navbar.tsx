265898f5e29e3111cf66a897e0df8c81bee4f799
RasulGhatane <rasulghatane@gmail.com>
2025-04-23 23:30:20 +0545

    home-layout fix

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -66,7 +66,7 @@ const navbarItems = [
 
 export const Navbar = () => {
     const pathname = usePathname();
-    const [isSidebarOpen, setSidebarOpen] = React.useState(true);
+    const [isSidebarOpen, setSidebarOpen] = React.useState(false);
 
     return (
         <nav className="h-20 flex border-b justify-between font-medium bg-white">
